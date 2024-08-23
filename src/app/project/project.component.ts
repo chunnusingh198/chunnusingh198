f54ffdb8b05d79a2cff1b428231992955ce61ff0
Chunnu Singh <chunnukumar198@gmail.com>
2024-08-23 23:11:01 +0530

    fix

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -14,13 +14,13 @@ export class ProjectComponent implements OnInit {
     heding:'MONET DIY',
     text:'Monet is an AI-based content performance analytics platform. It offers a solution for video content rating, message testing, non-verbal cues (NVCs), real-time emotion capture, content analytics, consumer insights, facial coding, creating strategy, and understanding consumer behavior.',
     image:'assets/image/monetdiy.png',
-    // url:'https://corporate.monetanalytics.com/'
+    url:'https://corporate.monetanalytics.com/'
   },
   {
     heding:'EEMO',
     text:'eemo is the safe place to try out your videos, win more hearts, and go from hope to dope in blazing speed. See how this patented emotionAI science maps facial expressions and predicts viewers emotional reactions, frame-by-frame, with profund accuracy.',
     image:'assets/image/eemo.png',
-    // url:'https://www.eemo.live/'
+    url:'https://www.eemo.live/'
   }
 ]
 
